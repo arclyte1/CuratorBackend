7f4f8d46be20e82be9f08fc03030306fe136a41f
arclyte1 <arclyte18@gmail.com>
2024-05-10 13:28:32 +0300

    added requests to admin

--- curator/admin.py
+++ curator/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from curator.models import User, Group, Student, Event
+from curator.models import User, Group, Student, Event, Request
 
 admin.site.register(User)
 admin.site.register(Group)
 admin.site.register(Student)
 admin.site.register(Event)
+admin.site.register(Request)
